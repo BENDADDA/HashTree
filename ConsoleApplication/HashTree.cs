dc97420caa1bbd82846c2c2645cdbe79cd85bebf
BENDADDA BENAOUDA <94899619+BENDADDA@users.noreply.github.com>
2022-12-25 23:42:51 -0800

    first commit

---
+++ ConsoleApplication/HashTree.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    class HashTree<TKey,TValue>
+    {
+        private class Root
+        {
+            public char Index;
+            public KeyValuePair<TKey, TValue>? keyValuePair;
+            public Root[] Childs = new Root[11];
+            public Root()
+            {
+                this.Index=' ';
+                this.keyValuePair = null;
+            }
+            public Root(char Index)
+            {
+                this.Index = Index;
+                this.keyValuePair = null;
+            }
+            public Root(char Index, TKey Key,TValue Value)
+            {
+                this.Index = Index;
+                this.keyValuePair = new KeyValuePair<TKey,TValue>(Key, Value);
+            }
+            
+        }
+        private Root root;
+        private int count;
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public HashTree()
+        {
+            this.root = new Root();
+            this.count = 0;
+        }
+        public void Add(TKey key,TValue value)
+        {
+            int hashCode=key.GetHashCode();
+            string path=hashCode.ToString();
+            Root tempRoot=root;
+            foreach(char letter in path)
+            {
+                int index = IndexOf(letter);
+                if (tempRoot.Childs[index] == null)
+                    tempRoot.Childs[index] = new Root(letter);
+                tempRoot = tempRoot.Childs[index];   
+            }
+            if(tempRoot.keyValuePair==null)
+            {
+                tempRoot.keyValuePair = new KeyValuePair<TKey, TValue>(key, value);
+                count++;
+            }
+        }
+        public TValue GetValue(TKey key)
+        {
+            Root tempRoot=root;
+            int hashCode=key.GetHashCode();
+            string path=hashCode.ToString();
+            foreach(char letter in path)
+            {
+                int index = IndexOf(letter);
+                if (tempRoot.Childs[index] == null) throw new Exception();
+                else
+                {
+                    tempRoot = tempRoot.Childs[index];
+                }
+              
+            }
+            if (tempRoot.keyValuePair == null) throw new Exception();
+            return tempRoot.keyValuePair.Value.Value;
+            
+        }
+        private int IndexOf(char letter)
+        {
+            int index= ((int)letter - (int)'0');
+            return index == -3 ? 10 : index;
+        }
+
+    }
+    
+    
+}
---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Matlab;
+using System.Text;
+namespace ConsoleApplication
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            HashTree<int, string> hashTree = new HashTree<int, string>();   
+            hashTree.Add(3, "Ben");
+            hashTree.Add(281, "Mohammed");
+            hashTree.Add(21, "Malik");
+            hashTree.Add(7, "Benaouda");
+            hashTree.Add(23, "Jack");
+            hashTree.Add(220, "Yassine");
+            hashTree.Add(2000, "Bendadda");
+            hashTree.Add(-39,"Chareli");
+            hashTree.Add(500, "Yoasaffe");
+            hashTree.Add(19, "Julliet");
+            hashTree.Add(10, "Claire");
+            hashTree.Add(92, "Scailare");
+            hashTree.Add(389, "Jimes");
+            hashTree.Add(-402, "Goodwin");     
+            Console.WriteLine("Count:"+hashTree.Count);
+            Console.WriteLine("Value:"+hashTree.GetValue(7));
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey();
+        }
+    }
+}
