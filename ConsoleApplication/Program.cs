26fffea55c3fc181446aef99764c5f6c6ae5a668
BENDADDA BENAOUDA <94899619+BENDADDA@users.noreply.github.com>
2022-12-29 04:26:56 -0800

    Update Program.cs

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Matlab;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Collections;
 namespace ConsoleApplication
 {
+
+   
     class Program
     {
        
         static void Main(string[] args)
         {
+            
             HashTree<int, string> hashTree = new HashTree<int, string>();   
             hashTree.Add(3, "Ben");
             hashTree.Add(281, "Mohammed");
@@ -21,12 +26,23 @@ namespace ConsoleApplication
             hashTree.Add(-39,"Chareli");
             hashTree.Add(500, "Yoasaffe");
             hashTree.Add(19, "Julliet");
+            hashTree.Add(0, "NULL");
             hashTree.Add(10, "Claire");
             hashTree.Add(92, "Scailare");
             hashTree.Add(389, "Jimes");
-            hashTree.Add(-402, "Goodwin");     
-            Console.WriteLine("Count:"+hashTree.Count);
-            Console.WriteLine("Value:"+hashTree.GetValue(7));
+            hashTree.Add(-402, "Goodwin");
+           
+            foreach (var pair in hashTree)
+            {
+                Console.WriteLine(pair);
+            }
+            string str;
+            hashTree.TryGetValue(2000, out str);
+            Console.WriteLine("value:"+str);
+           
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            Console.WriteLine(d);
+          
             Console.Write("\nPress any key to continue . . . ");
             Console.ReadKey();
         }
